b25559bc1ec71354646b4f7102a4889bf1ea5993
Alysson R. Elisiario <alyssonrafael5.512@gmail.com>
2025-04-23 19:41:11 -0300

    Add files via upload

--- exercicio13-aula5.py
+++ exercicio13-aula5.py
@@ -1,9 +1,11 @@
 salario = float(input('Coloque seu salario mensal: '))
-if salario <= 2000:
+if 0 < salario <= 2000:
     print('Seu salario está isento de imposto')
 elif 2000.01 <= salario <= 3500:
     salario = salario/10
-    print(f'Com o seu salario o valor ser pago de imposto é de {salario} ')
-else:
+    print(f'Com o seu salario o valor ser pago de imposto é de R${salario} ')
+elif 3500 < salario:
     salario = salario * 15 / 100
-    print(f'Com o seu salario o valor ser pago de imposto é de {salario} ')
+    print(f'Com o seu salario o valor ser pago de imposto é de R${salario} ')
+else:
+    print('Valor Invalido')
