a9c2677b85f99cbc69bd27170c89eb939aef920d
Alysson R. Elisiario <alyssonrafael5.512@gmail.com>
2025-04-23 20:46:50 -0300

    Add files via upload

--- Pesquisando em um vetor.py
+++ Pesquisando em um vetor.py
@@ -3,15 +3,22 @@ from random import randint
 q = []
 
 for x in range(20):
-    y = randint(0, 1000)
-    q.append(y)
+    numero = randint(0, 1000)
+    q.append(numero)
 
-maior = max(q)
-menor = min(q)
+maior = -1
+pos_maior = -1
+menor = 1000
+pos_menor = -1
 
-pos_maior = q.index(maior)+1
-pos_menor = q.index(menor)+1
+for i in range(len(q)):
+    if q[i] > maior:
+        maior = q[i]
+        pos_maior = i
+    if q[i] < menor:
+        menor = q[i]
+        pos_menor = i
 
 print(q)
-print(f'O Maior valor é {maior} e sua posição é {pos_maior}')
-print(f'O Menor valor é {menor} e sua posição é {pos_menor}')
+print(f'O maior valor é {maior} e sua posição é {pos_maior}')
+print(f'O menor valor é {menor} e sua posição é {pos_menor}')
